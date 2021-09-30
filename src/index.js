182c20ecef24a88e0de6f2e50070822809e6aca2
mhaslinsky <mhaslinsky@gmail.com>
2021-09-29 20:50:49 -0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import styled from "styled-components";
+
+ReactDOM.render(<App />, document.getElementById("root"));
